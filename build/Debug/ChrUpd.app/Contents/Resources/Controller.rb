63bf26e006de64162d172b958a7057bc03a76533
julienXX <julien.blanchard@me.com>
2009-12-04 17:08:12 +0100

    first commit

---
+++ build/Debug/ChrUpd.app/Contents/Resources/Controller.rb
@@ -0,0 +1,45 @@
+# Controller.rb
+# ChrUpd
+#
+# Created by Julien Blanchard on 04/12/09.
+# Copyright 2009 MIKROS IMAGE. All rights reserved.
+
+require 'net/http'
+require 'fileutils'
+
+class Controller
+	attr_writer :text_view
+	attr_accessor :progress
+	
+	def awakeFromNib()
+		@text_view.insertText("Click the Update button to update Chromium.\n")
+		#@progress = NSProgressIndicator.new
+		@build_url = "build.chromium.org"
+		@whoami = %x(whoami).gsub(/\n/,'')
+		
+		Net::HTTP.start(@build_url) { |http|
+			@latest = http.get("/buildbot/snapshots/chromium-rel-mac/LATEST")
+  	}
+	end
+	
+	def updateChromium(sender)
+		@progress.startAnimation(nil)
+		
+		# Download latest chromium
+		system("wget http://#{@build_url}/buildbot/snapshots/chromium-rel-mac/#{@latest.body}/chrome-mac.zip -O /Users/#{@whoami}/chrome-mac.zip")
+		@text_view.insertText("\nDownloaded Chromium revision #{@latest.body}, you can run it by clicking Run Chromium.\n")
+		
+		# Unzip and copy and remove temp files
+		system("cd /Users/#{@whoami} && unzip -qq chrome-mac.zip && cd chrome-mac && cp -fR Chromium.app /Applications && cd .. && rm -rf chrome*")
+		
+		@progress.stopAnimation(nil)
+	end
+	
+	def runChromium(sender)
+		system("open /Applications/Chromium.app")
+		exit()
+	end
+	
+end
+
+
---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ChrUpd
+//
+//  Created by Julien Blanchard on 04/12/09.
+//  Copyright MIKROS IMAGE 2009. All rights reserved.
+//
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
