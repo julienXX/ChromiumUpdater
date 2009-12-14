c11403d41cbfc8c010dbfda4e0f88c58b7fec31c
julienXX <julien.blanchard@me.com>
2009-12-14 14:15:12 +0100

    removed build directory

--- build/ChrUpd.build/Release/Embed.build/Script-4D1A51A410A4FF640046A98A.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# The macruby_deploy command-line tool with the --embed argument will make sure the MacRuby framework will be embedded in your .app. Default options will be used. Pass the -h option to get more information.
-PATH="$PATH:/usr/local/bin" macruby_deploy --embed "$TARGET_BUILD_DIR/$PROJECT_NAME.app"
